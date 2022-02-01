abb5a87260fb8db940471ad82fb0529d8f2e65c8
GuiQRSM <endbeob15@gmail.com>
2022-02-01 13:00:19 -0300

    Instantiates the 'MainFrame' class in the 'home:,' attribute of 'MaterialApp()'

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:project_packages/Main_Frame.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: MainFrame(),
     ),
   );
 }
