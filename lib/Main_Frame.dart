077fc4a59fa2cb72deb9a4cb8b7932bd48e5f4cd
GuiQRSM <endbeob15@gmail.com>
2022-02-03 13:42:46 -0300

    Implement The 'stfu' structure of 'MainFrame' the element 'List item=[];' and the 'getItem() { }' method that receives in its braces '{ }' a 'Map<>' structure

--- lib/Main_Frame.dart
+++ lib/Main_Frame.dart
@@ -11,7 +11,19 @@ class _MainFrameState extends State<MainFrame> {
 
   var mainColor = Color.fromARGB(224, 123, 57, 0);
   var secColor = Color.fromARGB(255, 179, 128, 0);
-  var _itemIndex = 3;
+
+  List _item = [];
+  getItem() {
+
+    for(int i = 0; i >= 20; i++){
+
+      Map<String, dynamic> bodyItem = Map();
+      bodyItem ["content"] = "teste: ${i} ";
+      _item.add(bodyItem);
+
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,11 +44,17 @@ class _MainFrameState extends State<MainFrame> {
         child: SingleChildScrollView(
           padding: EdgeInsets.all(16),
           child: ListView.builder(
-    itemCount: _itemIndex.bitLength,
-    itemBuilder: (context, index) {},
+    itemCount: _indexItem.length,
+    itemBuilder: (context, index) {
+
+      return ListTile(
+        title: Text("${index.toString()}"),
+      );
+
+    },
     ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      /*floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton.extended(
         icon: Icon(
           Icons.add_rounded,
@@ -53,7 +71,8 @@ class _MainFrameState extends State<MainFrame> {
         backgroundColor: mainColor,
         onPressed: (){
         },
-      ),
+      ),*/
+     ),
     );
   }
 }
